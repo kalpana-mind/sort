7a51d2f442d8991b12a2a3db66ac0084ed233efa
kalpana-mind <152434265+kalpana-mind@users.noreply.github.com>
2024-08-08 13:36:26 -0400

    "Added abstract classes"

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,13 @@ import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
-// const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
-// numbersCollection.sort();
-// console.log(numbersCollection.data);
+const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
-// const charactersCollection = new CharactersCollection('Xaayb');
-// charactersCollection.sort();
-// console.log(charactersCollection.data);
+const charactersCollection = new CharactersCollection('Xaayb');
+charactersCollection.sort();
+console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
 linkedList.add(500);
